22c44ea453b7a9115aa3de0c41d4c155e8ed7a88
Zahra Malikzada <zahra.malikzada04@gmail.com>
2025-12-24 23:58:06 +0400

    Adding some functionalities

--- src/Homework2512/Main.java
+++ src/Homework2512/Main.java
@@ -1,9 +1,29 @@
 package Homework2512;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CheckArray.checkValue("Azerbaijan");
-        CheckArray.checkValue("Danimark");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while(true){
+            System.out.print("Enter country: ");
+            String country = sc.next();
+
+            CheckArray.checkValue(country);
+
+            System.out.print("Do ypu want to continue (y/n): ");
+            String answer = sc.next();
+
+            if("y".equals(answer)){
+                continue;
+            }
+            else{
+                break;
+            }
+        }
 
 
     }
